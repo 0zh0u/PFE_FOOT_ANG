633baf6c009b8cfcbc2cd816be4208ce53eaf9e8
0zh0u <olivierzhou1992@gmail.com>
2017-08-30 15:09:20 +0200

    Test bouton créer event

--- imports/components/dashboard/dashboard.js
+++ imports/components/dashboard/dashboard.js
@@ -6,6 +6,8 @@ import "fullcalendar/dist/fullcalendar.min.css";
 import "fullcalendar/dist/fullcalendar.print.min.css";
 import {name as Navig} from "../navig/navig";
 import {name as PlayerRow} from "../playerRow/playerRow";
+import {name as EventCreateCtrl} from "../eventCreate/eventCreate";
+import {name as EventUpdateCtrl} from "../eventUpdate/eventUpdate";
 import {Teams} from "../../api/Teams";
 
 import template from "./dashboard.html";
