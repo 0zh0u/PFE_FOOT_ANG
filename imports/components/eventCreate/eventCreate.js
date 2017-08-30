fc9409e4e0a7dd517dd6d3816d977b9dc07ec458
0zh0u <olivierzhou1992@gmail.com>
2017-08-30 14:54:16 +0200

    Mise à jour script JS de création event

--- imports/components/eventCreate/eventCreate.js
+++ imports/components/eventCreate/eventCreate.js
@@ -4,4 +4,44 @@ import angularMeteor from "angular-meteor";
 
 import template from "./eventCreate.html";
 
+const INITIAL_EVENT = {
+    _id: 'tocreate',
+    city: 'Ville',
+    location: 'Stade de France',
+	type: 'Match',
+    date: new Date().getTime(),
+};
+
+class EventCreateCtrl {
+    constructor($scope,$mdDialog) {
+
+        $scope.viewModel(this);
+
+        this.event = INITIAL_EVENT;
+
+        this.closeDialog = function() {
+            $mdDialog.hide();
+        };
+    }
+
+    createEvent() {
+        Meteor.call('teams.createEvent', this.teamId, angular.copy(this.event));
+
+        this.event = INITIAL_EVENT;
+        this.closeDialog();
+    }
+}
+
 const name = 'eventCreate'
+
+export default angular.module(name, [
+    angularMeteor
+])
+    .component(name, {
+        templateUrl: template,
+        controllerAs: name,
+        controller: ['$scope','$mdDialog', EventCreateCtrl],
+        bindings: {
+            teamId: "<"
+        }
+    });
