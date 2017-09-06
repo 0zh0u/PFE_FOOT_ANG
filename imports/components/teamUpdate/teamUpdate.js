4a96fd0212a90773766b52fdf0de3ee730989b1c
0zh0u <olivierzhou1992@gmail.com>
2017-09-06 21:30:03 +0200

    Ajout structure pour update équipes

---
+++ imports/components/teamUpdate/teamUpdate.js
@@ -0,0 +1,13 @@
+import angular from "angular";
+import angularMeteor from "angular-meteor";
+import uiRouter from "angular-ui-router";
+import {Teams} from "../../api/Teams";
+
+import template from "./teamUpdate.html";
+
+class teamUpdateCtrl {
+
+}
+
+const name = 'teamUpdate'
+
